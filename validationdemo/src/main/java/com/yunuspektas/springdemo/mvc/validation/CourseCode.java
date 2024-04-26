a9b42b6f79f1c07f583911d3af2e69f5f9319026
Yunus Pektaş <145374270+yunuspektass@users.noreply.github.com>
2024-04-26 21:43:51 +0300

    Spring MVC Form Validation
    We validated the customer form using Spring MVC Validation and HTML, coded our own custom rules with Spring MVC, and took care of the backend side of the form. We used annotations and created our own custom annotation.

---
+++ validationdemo/src/main/java/com/yunuspektas/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,31 @@
+package com.yunuspektas.springdemo.mvc.validation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD , ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+//    define default course code
+    public String value() default  "YNS";
+
+//    define default error message
+  public String message() default "must start with YNS";
+
+//    define default groups
+    public Class<?> [] groups() default {};
+
+//    define default payloads
+ public Class<? extends Payload>[] payload() default {};
+
+
+
+}
